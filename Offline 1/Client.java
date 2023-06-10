b77170705bd34da9e7768121a93a46e053af9809
Sadif-Ahmed <1905058@ugrad.cse.buet.ac.bd>
2023-06-10 20:32:04 +0600

    No Significant Changes

--- Offline 1/Client.java
+++ Offline 1/Client.java
@@ -31,7 +31,7 @@ import java.io.*;
                 } catch (Exception e) {
                     //TODO: handle exception
                 }
-                //mat[i][j]=in.nextInt();
+               
             }
           }
           System.out.println("Initial Board State: ");
@@ -76,33 +76,5 @@ import java.io.*;
         }
           
       
-      
-
-      //for lightoz test cases
-       /*
-      int n = in.nextInt();
-      int [][]mat  = new int[4][4];
-      int tc=0;
-      while(n>0){
-        for (int i = 1; i <= 3; i++) {
-            for (int j = 1; j <= 3; j++) {
-                mat[i][j] = in.nextInt();
-            }
-        }
-        String s = in.nextLine();
-
-        board_solver bs = new board_solver(mat , 3);
-        bs.set_heuristic_id(3);
-        Node goal = bs.solve();
-        if(goal != null){
-            System.out.println("Case "+ (++tc) + ": " + goal.f_cost);
-            //bs.print_path(goal);
-        }
-        else{
-            System.out.println("Case "+ (++tc) + ": impossible");
-        }
-        n--;  
-      }
-      */
     }  
  }
