080ff5f07e6319ac49f5d3ce73af124dc50a6589
Sadif-Ahmed <ahmedsadif67@gmail.com>
2023-07-17 11:28:57 +0600

    Refact..

--- Offline 2/Algo_Heuristics.java
+++ Offline 2/Algo_Heuristics.java
@@ -1,34 +1,32 @@
 import java.util.ArrayList;
 import java.util.Random;
-class Opt_Node {
+public class Algo_Heuristics {
     Tree_Node node;
     double h_value;
+    static final double INF = 10000000;
 
-    Opt_Node(Tree_Node node,double h_value)
+    Algo_Heuristics(Tree_Node node,double h_value)
     {
         this.node=node;
         this.h_value=h_value;
     }
-    public static Opt_Node find_max(Opt_Node node1,Opt_Node node2)
+    public static Algo_Heuristics find_max(Algo_Heuristics node1,Algo_Heuristics node2)
     {
         if(node2==null) return node1;
         else if(node1!=null && node1.h_value >= node2.h_value) return node1;
         else return node2;
     }
-    public static Opt_Node find_min(Opt_Node node1,Opt_Node node2)
+    public static Algo_Heuristics find_min(Algo_Heuristics node1,Algo_Heuristics node2)
     {
         if(node2==null) return node1;
         else if(node1!=null && node1.h_value <= node2.h_value) return node1;
         else return node2;
     }
-}
 
-public class Algo_Heuristics {
-    static final double INF = 10000000;
 
    //returns the optimal next bin idx
 	public static int mini_max(Tree_Node root , int depth) {
-		Opt_Node opt = alpha_beta( root , -INF , INF , true , depth );
+		Algo_Heuristics opt = alpha_beta( root , -INF , INF , true , depth );
 		ArrayList< Tree_Node > list = root.get_successor_list();
 		
 		for (int i=0 ; i<list.size() ; i++){
@@ -37,51 +35,53 @@ public class Algo_Heuristics {
 		}
 		return -1;
 	}
+    public static ArrayList<Algo_Heuristics> find_best(ArrayList<Algo_Heuristics> candidates,Algo_Heuristics reference)
+    {
+        ArrayList<Algo_Heuristics> bestlist = new ArrayList<Algo_Heuristics>();
+        for(int i=0;i<candidates.size();i++)
+        {
+            if(candidates.get(i).h_value==reference.h_value)
+            {
+                bestlist.add(candidates.get(i));
+            }
+        }
+        return bestlist;
+    }
 
-    public static Opt_Node alpha_beta(Tree_Node state , double alpha , double beta , boolean maximizing_check , int maxdepth ) {
+    public static Algo_Heuristics alpha_beta(Tree_Node state , double alpha , double beta , boolean maximizing_check , int maxdepth ) {
 		if (state.terminal_check() || maxdepth == 0){
 			double h_val = state.heuristic_value();
-			return new Opt_Node(state , h_val);
+			return new Algo_Heuristics(state , h_val);
 		}
 		if (maximizing_check){
-			ArrayList<Opt_Node>ls = new ArrayList<Opt_Node>();//to store all maxNodes
-			Opt_Node maxNode = new Opt_Node( null , -INF );
+			ArrayList<Algo_Heuristics>max_node_list = new ArrayList<Algo_Heuristics>();//to store all maxNodes
+			Algo_Heuristics max_node = new Algo_Heuristics( null , -INF );
 			for (Tree_Node s : state.get_successor_list()){
 				if (s == null) continue;
-				Opt_Node tmp_node = new Opt_Node(s , alpha_beta(s , alpha , beta , s.maximizing_check() , maxdepth - 1 ).h_value );
-				maxNode = Opt_Node.find_max(maxNode , tmp_node);
-				ls.add(maxNode);
-				alpha = Math.max( alpha , maxNode.h_value );
+				Algo_Heuristics tmp_node = new Algo_Heuristics(s , alpha_beta(s , alpha , beta , s.maximizing_check() , maxdepth - 1 ).h_value );
+				max_node = find_max(max_node , tmp_node);
+				max_node_list.add(max_node);
+				alpha = Math.max( alpha , max_node.h_value );
 				if (alpha >= beta) break; //pruning
 			}
-			ArrayList<Opt_Node>best = new ArrayList<Opt_Node>();//to store best nodes if multiple
-			for(int i=0 ; i<ls.size(); i++){
-				if(ls.get(i).h_value == maxNode.h_value){
-					best.add(ls.get(i));
-				}
-			}
-			return best.get(new Random().nextInt(best.size()));//choose a best node randomly
-
-			
+			ArrayList<Algo_Heuristics>best_list = new ArrayList<Algo_Heuristics>();//to store best nodes if multiple
+			best_list=find_best(max_node_list, max_node);
+			return best_list.get(new Random().nextInt(best_list.size()));//choose a best node randomly	
 		}
 		else{
-			Opt_Node minNode = new Opt_Node( null , INF );
-			ArrayList<Opt_Node>ls = new ArrayList<Opt_Node>();
+			ArrayList<Algo_Heuristics>min_node_list = new ArrayList<Algo_Heuristics>();
+            Algo_Heuristics min_node = new Algo_Heuristics( null , INF );
 			for (Tree_Node s : state.get_successor_list()) {
 				if (s == null) continue;
-				Opt_Node tmp_node = new Opt_Node( s , alpha_beta( s , alpha , beta , s.maximizing_check() , maxdepth - 1 ).h_value);
-				minNode = Opt_Node.find_min(minNode , tmp_node);
-				ls.add(minNode);
-				beta = Math.min( beta , minNode.h_value);
+				Algo_Heuristics tmp_node = new Algo_Heuristics( s , alpha_beta( s , alpha , beta , s.maximizing_check() , maxdepth - 1 ).h_value);
+				min_node = find_min(min_node , tmp_node);
+				min_node_list.add(min_node);
+				beta = Math.min( beta , min_node.h_value);
 				if (alpha >= beta) break; //pruning
 			}
-			ArrayList<Opt_Node>best = new ArrayList<Opt_Node>();
-			for(int i=0 ; i<ls.size(); i++){
-				if(ls.get(i).h_value == minNode.h_value){
-					best.add(ls.get(i));
-				}
-			}
-			return best.get(new Random().nextInt(best.size()));
+			ArrayList<Algo_Heuristics>best_list = new ArrayList<Algo_Heuristics>();//to store best nodes if multiple
+			best_list=find_best(min_node_list, min_node);
+			return best_list.get(new Random().nextInt(best_list.size()));
 		}
 }
 public static int get_h_value(Game_Board bd,int h_id){
